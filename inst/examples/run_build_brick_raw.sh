e06197c43f2c2a76e9b39cae9f8908deef64617e
albhasan <albhasan@gmail.com>
2019-06-27 15:58:18 -0300

    Code tested and ran.

--- inst/examples/run_build_brick_raw.sh
+++ inst/examples/run_build_brick_raw.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#Rscript build_brick_raw.R --scene "225063" --from "2016-08-01" --to "2017-08-20"
-#Rscript build_brick_raw.R --scene "225063" --from "2015-08-01" --to "2016-08-20"
-#Rscript build_brick_raw.R --scene "225063" --from "2014-08-01" --to "2015-08-20"
-#Rscript build_brick_raw.R --scene "225063" --from "2013-08-01" --to "2014-08-20"
+Rscript build_brick_raw.R --scene "225063" --from "2016-08-01" --to "2017-08-20"
+Rscript build_brick_raw.R --scene "225063" --from "2015-08-01" --to "2016-08-20"
+Rscript build_brick_raw.R --scene "225063" --from "2014-08-01" --to "2015-08-20"
+Rscript build_brick_raw.R --scene "225063" --from "2013-08-01" --to "2014-08-20"
 
-#Rscript build_brick_raw.R --scene "226064" --from "2016-08-01" --to "2017-08-20"
-#Rscript build_brick_raw.R --scene "226064" --from "2015-08-01" --to "2016-08-20"
-#Rscript build_brick_raw.R --scene "226064" --from "2014-08-01" --to "2015-08-20"
-#Rscript build_brick_raw.R --scene "226064" --from "2013-08-01" --to "2014-08-20"
+Rscript build_brick_raw.R --scene "226064" --from "2016-08-01" --to "2017-08-20"
+Rscript build_brick_raw.R --scene "226064" --from "2015-08-01" --to "2016-08-20"
+Rscript build_brick_raw.R --scene "226064" --from "2014-08-01" --to "2015-08-20"
+Rscript build_brick_raw.R --scene "226064" --from "2013-08-01" --to "2014-08-20"
 
 Rscript build_brick_raw.R --scene "233067" --from "2016-08-01" --to "2017-08-30"
 Rscript build_brick_raw.R --scene "233067" --from "2015-08-01" --to "2016-08-30"
