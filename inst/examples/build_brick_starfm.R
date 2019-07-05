a16033837e2e217c84329c930e21d61245ab516e
albhasan <albhasan@gmail.com>
2019-07-05 16:31:49 -0300

    It now takes parameters from the terminal. Code for debuggin (dump file) removed.

--- inst/examples/build_brick_starfm.R
+++ inst/examples/build_brick_starfm.R
@@ -1,39 +1,41 @@
 #!/usr/bin/env Rscript
 
-# build a brick using Landsat 8 and MOD13Q1 images
-
-library(devtools)
+# Build a brick using Landsat 8 and  MOD13Q1 through the STARFM model.
 setwd("/home/alber/Documents/data/experiments/l8mod-fusion/Rpackage/sits.starfm")
-devtools::load_all()
+suppressMessages(suppressPackageStartupMessages(library(dplyr)))
+suppressMessages(suppressPackageStartupMessages(library(optparse)))
+library(sits.starfm)
 
-dump_file <- file.path(paste0("last.dump_", stringr::str_replace(system('uname -n', intern = TRUE), "-", "_")))
-dump_and_quit <- function() {
-    # Save debugging info to file last.dump.rda
-    dump.frames(dumpto = dump_file, to.file = TRUE)
-    # Quit R with error status
-    q(status = 1)
+option_list = list(
+    make_option(c("-s", "--scene"), type = "character", default = NULL, 
+        help = "landsat scene e.g. 225063",        metavar = "character"),
+    make_option(c("-y", "--year"),    type = "integer", default = "NULL", 
+        help = "brick's PRODES year e.g. 2016",   metavar = "character")
+)
+opt_parser <- OptionParser(option_list = option_list);
+opt <- parse_args(opt_parser)
+
+if (any(is.null(opt$scene), is.null(opt$year))) {
+    print_help(opt_parser)
+    stop("Invalid input!", call.=FALSE)
 }
-options(error = dump_and_quit)
-# In a later interactive session ----
-# load(dump_file)
-# debugger()
-#options( warn = 2 ) # TODO: remove for production
 
-#debugonce(run_starFM)
-brick_233067_2015 <- build_brick_starfm(landsat_path    = "/home/alber/landsat8",
-                                        modis_path      = "/home/alber/MOD13Q1", 
-                                        scene_shp       = "/home/alber/Documents/data/experiments/l8mod-fusion/data/shp/wrs2_descending.shp",
-                                        tile_shp        = "/home/alber/Documents/data/experiments/l8mod-fusion/data/shp/modis-tiles.shp",
-                                        brick_scene     = "225063",
-                                        brick_year      = 2017,
-                                        brick_bands     = c("sr_band4", "sr_band5", "sr_band7"),
-                                        brick_path      = "/home/alber/shared/brick",
-                                        cloud_threshold = 0.99,
-                                        n_best_img      = 4,
-                                        img_per_year    = 23,
-                                        image_step      = ceiling(365/img_per_year),
-                                        temp_dir        = "/home/alber/shared/tmp")
+brick_scene <- opt$scene
+brick_year  <- opt$year
 
-warnings()
-options(error = NULL)
+img_per_year <- 23
+build_brick_starfm(landsat_path    = "/home/alber/landsat8",
+                   modis_path      = "/home/alber/MOD13Q1", 
+                   scene_shp       = "/home/alber/Documents/data/experiments/l8mod-fusion/data/shp/wrs2_descending.shp",
+                   tile_shp        = "/home/alber/Documents/data/experiments/l8mod-fusion/data/shp/modis-tiles.shp",
+                   brick_scene     = brick_scene,
+                   brick_year      = brick_year,
+                   brick_bands     = c("sr_band4", "sr_band5", "sr_band7"),
+                   brick_path      = "/home/alber/Documents/data/experiments/prodes_reproduction/data/raster/brick_starfm_v2",
+                   cloud_threshold = 0.01,
+                   n_best_img      = img_per_year,
+                   img_per_year    = img_per_year,
+                   image_step      = ceiling(365/img_per_year))
+
+print(sprintf("Finished processing brick starfm %s %s", brick_scene, brick_year))
 
