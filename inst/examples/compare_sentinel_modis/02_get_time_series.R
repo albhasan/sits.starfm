d247f3222d6a93175abe8ccedf37f0b16cfb61f7
albhasan <albhasan@gmail.com>
2020-05-18 22:39:39 -0300

    Added code for the new adjusted samples.

--- inst/examples/compare_sentinel_modis/02_get_time_series.R
+++ inst/examples/compare_sentinel_modis/02_get_time_series.R
@@ -16,15 +16,8 @@ raster::tmpDir()
 source("/home/alber/Documents/ghProjects/sits.starfm/inst/examples/compare_sentinel_modis/util.R")
 
 sample_shp <- "/home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/samples/samples_20LKP_v2.shp"
-
-raw_brick_path    <- "/home/alber/Documents/data/experiments/prodes_reproduction/data/raster/brick_sentinel2_raw"
-porous_brick_path <- "/home/alber/Documents/data/experiments/prodes_reproduction/data/raster/brick_sentinel2_interpolated/porous"
 approx_brick_path <- "/home/alber/Documents/data/experiments/prodes_reproduction/data/raster/brick_sentinel2_interpolated/approx"
 
-#raw_brick_tb <- raw_brick_path %>%
-#    get_brick()
-#porous_brick_tb <- porous_brick_path %>%
-#    get_brick()
 approx_brick_tb <- approx_brick_path %>%
     get_brick() %>%
     dplyr::filter(resolution == "10m") %>%
@@ -33,18 +26,6 @@ approx_brick_tb <- approx_brick_path %>%
     ensurer::ensure_that(!any(is.na(.$band)), err_desc = "Unknown bands!")
 
 # Create a coverage for each brick.
-#raw_cube <- sits::sits_cube(service = "BRICK", name = "sentinel-bricks",
-#                            satellite = "SENTINEL2", sensor = "MSI",
-#                            timeline = seq(unique(raw_brick_tb$img_date),
-#                                           by = 10, length.out = 36) ,
-#                            bands = raw_brick_tb$band,
-#                            files = raw_brick_tb$file_path)
-#porous_cube <- sits::sits_cube(service = "BRICK", name = "sentinel-bricks",
-#                            satellite = "SENTINEL2", sensor = "MSI",
-#                            timeline = seq(unique(porous_brick_tb$img_date),
-#                                           by = 10, length.out = 36) ,
-#                            bands = porous_brick_tb$band,
-#                            files = porous_brick_tb$file_path)
 approx_cube <- sits::sits_cube(service = "BRICK", name = "sentinel-bricks",
                             satellite = "SENTINEL2", sensor = "MSI",
                             timeline = seq(unique(approx_brick_tb$img_date),
@@ -56,15 +37,7 @@ approx_cube <- sits::sits_cube(service = "BRICK", name = "sentinel-bricks",
 # NOTE: This is necessary because sits::sits_get_data ignores the shp's labels.
 sample_csv <- sample_shp %>%
     sf::read_sf() %>%
-    (function(x){
-         xy <- x %>%
-             sf::st_coordinates() %>%
-             magrittr::set_colnames(c("longitude", "latitude")) %>%
-             tidyr::as_tibble()
-         x %>%
-             dplyr::bind_cols(xy) %>%
-             return()
-    }) %>%
+    add_coords() %>%
     sf::st_set_geometry(NULL) %>%
     dplyr::rename(label = Label) %>%
     dplyr::mutate(start_date = "2018-08-01",
@@ -77,8 +50,6 @@ sample_csv <- sample_shp %>%
     })
 
 # Get the time series for each sample.
-#raw_samples_tb    <- sits::sits_get_data(raw_cube,    file = sample_csv)
-#porous_samples_tb <- sits::sits_get_data(porous_cube, file = sample_csv)
 approx_samples_tb <- sits::sits_get_data(approx_cube, file = sample_csv)
 
 # Report invalid samples.
@@ -114,5 +85,93 @@ approx_samples_tb %>%
     saveRDS(file = "/home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/samples/approx_samples_tb_v2.rds")
 
 
-#saveRDS(raw_samples_tb   , file = "/home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/samples/raw_samples_tb_v2.rds")
-#saveRDS(porous_samples_tb, file = "/home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/samples/porous_samples_tb_v2.rds")
+
+################################################################################
+# SAMPLES FOR THE FIRST CLASSIFICATION
+################################################################################
+
+# Export SHP to CSV.
+sample4first_shp <- "/home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/validation_tudo_final4.shp"
+
+samples4first_csv <- sample4first_shp %>%
+    ensurer::ensure_that(file.exists(.), err_desc = "File not found!") %>%
+    sf::read_sf() %>%
+    sf::st_transform(crs = 4326L) %>%
+    add_coords() %>%
+    sf::st_set_geometry(NULL) %>%
+    dplyr::mutate(label = dplyr::recode(label,
+                                            Deforestatio = "Deforestation",
+                                            Forest       = "Forest",
+                                            NatNonForest = "Natural_Non-Forest",
+                                            NonFerest    = "Non-Forest",
+                                            NonForest    = "Non-Forest",
+                                            Pasture      = "Pasture",
+                                            Water        = "Water")) %>%
+    tidyr::drop_na() %>%
+    ensurer::ensure_that(length(unique(.$label)) == 6,
+                         err_desc = sprintf("Unknown labels found: %s",
+                                            paste(sort(unique(.$label))))) %>%
+    ensurer::ensure_that(!all(is.na(.$label)),
+                         err_desc = "Labels cannot be NA!") %>%
+    dplyr::mutate(start_date  = "2018-08-01",
+                  end_date    = "2019-07-31",
+                  coverage    = layer,
+                  time_series = NA) %>%
+    ensurer::ensure_that(!all(is.na(.$coverage)),
+                         err_desc = "Coverage must describe the samples' origin file!") %>%
+    dplyr::select(longitude, latitude, start_date,
+                  end_date, label, coverage, time_series) %>%
+    dplyr::mutate(start_date = "2018-08-01",
+                  end_date   = "2019-07-31") %>%
+    (function(x){
+        csv_file <- tempfile(pattern = "samples4first_shp2csv_", fileext = ".csv")
+        readr::write_csv(x, path = csv_file)
+        return(csv_file)
+    })
+
+# Get the time series for each sample.
+samples4first_tb <- sits::sits_get_data(approx_cube, file = samples4first_csv)
+
+# Report invalid samples.
+samples4first_tb %>%
+    clean_ts(report = TRUE) %>%
+    (function(x){
+         print("Bands available:")
+         print(sort(sits::sits_bands(x)))
+         print("Number of samples:")
+         print(nrow(x))
+         invisible(x)
+    }) %>%
+    dplyr::mutate(na_col   = purrr::map(time_series, function(x){
+                  na_vec <- sapply(lapply(x, is.na), sum)
+                  return(names(na_vec[na_vec > 0]))
+                })) %>%
+    dplyr::pull(na_col) %>%
+    unlist() %>%
+    table() %>%
+    print()
+
+# NOTE: Some NAs in NDMI were interpolated.
+samples4first_tb$time_series[[ 41]]$ndmi <- approx(samples4first_tb$time_series[[ 41]]$ndmi, n = 36)$y
+samples4first_tb$time_series[[382]]$ndmi <- approx(samples4first_tb$time_series[[382]]$ndmi, n = 36)$y
+samples4first_tb$time_series[[441]]$ndmi <- approx(samples4first_tb$time_series[[441]]$ndmi, n = 36)$y
+
+original_layer <- sample4first_shp %>%
+    sf::read_sf() %>%
+    sf::st_transform(crs = 4326L) %>%
+    add_coords() %>%
+    sf::st_set_geometry(NULL) %>%
+    dplyr::mutate(lon6 = round(longitude * 1000000),
+                  lat6 = round(latitude  * 1000000)) %>%
+    dplyr::select(lon6, lat6, layer)
+
+samples4first_tb %>%
+    dplyr::mutate(lon6 = round(longitude * 1000000),
+                  lat6 = round(latitude  * 1000000)) %>%
+    dplyr::left_join(original_layer, by = c("lon6", "lat6")) %>%
+    ensurer::ensure_that(!all(is.na(.$layer))) %>%
+    dplyr::mutate(cube = layer) %>%
+    dplyr::select(-lon6, -lat6, -layer) %>%
+    ensurer::ensure_that(!all(is.na(.$cube))) %>%
+    saveRDS(file = "/home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/validation_tudo_final4.rds")
+
