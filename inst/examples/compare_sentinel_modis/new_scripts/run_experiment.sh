e0aa2ebdbd0fedb659fd8bea0fedcc218a94aef2
albhasan <albhasan@gmail.com>
2020-05-27 10:04:42 -0300

    Updated to new script signatures.

--- inst/examples/compare_sentinel_modis/new_scripts/run_experiment.sh
+++ inst/examples/compare_sentinel_modis/new_scripts/run_experiment.sh
@@ -2,7 +2,6 @@
 
 script_dir="/home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/scripts_bricks/new_scripts"
 
-
 #---- Build bricks ----
 
 # Build VRTs from the bands.
@@ -30,56 +29,53 @@ parallel -j 8 "${script_dir}"/01_build_bricks/./build_tif_brick.sh ::: B02_maske
 #---- Interpolate masked brikcs ----
 
 #TODO: fix paths.
-Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B02_masked_10m.tif  /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B02_approx_10m.tif 
-Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B03_masked_10m.tif  /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B03_approx_10m.tif
-Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B04_masked_10m.tif  /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B04_approx_10m.tif
-Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B08_masked_10m.tif  /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B08_approx_10m.tif
-Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B11_masked_10m.tif  /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B11_approx_10m.tif
-Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B12_masked_10m.tif  /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B12_approx_10m.tif
-Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B8A_masked_10m.tif  /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B8A_approx_10m.tif
-Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_evi_masked_10m.tif  /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_evi_approx_10m.tif
-Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_ndmi_masked_10m.tif /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_ndmi_approx_10m.tif
-Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_ndvi_masked_10m.tif /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_ndvi_approx_10m.tif
-Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_savi_masked_10m.tif /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_savi_approx_10m.tif
+Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B02_masked_10m.tif  /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B02_approx_10m.tif 
+Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B03_masked_10m.tif  /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B03_approx_10m.tif
+Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B04_masked_10m.tif  /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B04_approx_10m.tif
+Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B08_masked_10m.tif  /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B08_approx_10m.tif
+Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B11_masked_10m.tif  /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B11_approx_10m.tif
+Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B12_masked_10m.tif  /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B12_approx_10m.tif
+Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B8A_masked_10m.tif  /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B8A_approx_10m.tif
+Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_evi_masked_10m.tif  /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_evi_approx_10m.tif
+Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_ndmi_masked_10m.tif /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_ndmi_approx_10m.tif
+Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_ndvi_masked_10m.tif /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_ndvi_approx_10m.tif
+Rscript "${script_dir}"/01_build_bricks/interp_sentinel-2.R approx /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_savi_masked_10m.tif /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_savi_approx_10m.tif
 
 #---- Bricks of the first two images ----
 
-/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2_raw/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B02_approx_10m.vrt  /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B02_approx_10m.tif 
-/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2_raw/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B03_approx_10m.vrt  /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B03_approx_10m.tif 
-/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2_raw/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B04_approx_10m.vrt  /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B04_approx_10m.tif 
-/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2_raw/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B08_approx_10m.vrt  /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B08_approx_10m.tif 
-/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2_raw/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B11_approx_10m.vrt  /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B11_approx_10m.tif 
-/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2_raw/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B12_approx_10m.vrt  /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B12_approx_10m.tif 
-/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2_raw/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B8A_approx_10m.vrt  /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_B8A_approx_10m.tif 
-/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2_raw/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_evi_approx_10m.vrt  /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_evi_approx_10m.tif 
-/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2_raw/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_ndmi_approx_10m.vrt /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_ndmi_approx_10m.tif
-/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2_raw/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_ndvi_approx_10m.vrt /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_ndvi_approx_10m.tif
-/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2_raw/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_savi_approx_10m.vrt /disks/d3/brick_sentinel2_raw/S2A_MSIL2A_R096_T20LKP_20180812T143751_savi_approx_10m.tif
+/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B02_approx_10m.vrt  /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B02_approx_10m.tif 
+/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B03_approx_10m.vrt  /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B03_approx_10m.tif 
+/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B04_approx_10m.vrt  /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B04_approx_10m.tif 
+/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B08_approx_10m.vrt  /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B08_approx_10m.tif 
+/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B11_approx_10m.vrt  /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B11_approx_10m.tif 
+/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B12_approx_10m.vrt  /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B12_approx_10m.tif 
+/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B8A_approx_10m.vrt  /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_B8A_approx_10m.tif 
+/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_evi_approx_10m.vrt  /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_evi_approx_10m.tif 
+/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_ndmi_approx_10m.vrt /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_ndmi_approx_10m.tif
+/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_ndvi_approx_10m.vrt /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_ndvi_approx_10m.tif
+/usr/bin/gdalbuildvrt -b 1 -b 2 /disks/d3/brick_sentinel2/first_2/S2A_MSIL2A_R096_T20LKP_20180812T143751_savi_approx_10m.vrt /disks/d3/brick_sentinel2/S2A_MSIL2A_R096_T20LKP_20180812T143751_savi_approx_10m.tif
 
 #---- Get time series ----
 
-"${script_dir}"/02_get_time_series/02_get_time_series.R /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_all_bands.csv approx /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/approx_samples_all_bands_csv.rds &
+"${script_dir}"/02_get_time_series/02_get_time_series.R /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_all_bands.csv approx /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_A_approx.rds & # approx_samples_all_bands_csv.rds &
 sleep 10
-"${script_dir}"/02_get_time_series/02_get_time_series.R /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_indices.csv   approx /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/approx_samples_indices_csv.rds
+"${script_dir}"/02_get_time_series/02_get_time_series.R /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_indices.csv   approx /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_B_approx.rds # approx_samples_indices_csv.rds
 
-"${script_dir}"/02_get_time_series/02_get_time_series.R /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_all_bands.csv raw /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/raw_samples_all_bands_csv.rds &
+"${script_dir}"/02_get_time_series/02_get_time_series.R /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_all_bands.csv raw /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_A_raw.rds & # raw_samples_all_bands_csv.rds &
 sleep 10
-"${script_dir}"/02_get_time_series/02_get_time_series.R /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_indices.csv   raw /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/raw_samples_indices_csv.rds
+"${script_dir}"/02_get_time_series/02_get_time_series.R /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_indices.csv   raw /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_B_raw.rds # raw_samples_indices_csv.rds
 
 #---- Compute K-Folds ----
 
-#TODO run again 
-"${script_dir}"/03_kfolds/03a_k-folds_analysis.R /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/approx_samples_all_bands_csv.rds /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/approx_samples_indices_csv.rds  /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/plot/kfold_approx
-
-#TODO kfolds of raw samples (save with anoteher name)
-"${script_dir}"/03_kfolds/03a_k-folds_analysis.R /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/raw_samples_all_bands_csv.rds /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/raw_samples_indices_csv.rds /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/plot/kfold_raw
+"${script_dir}"/03_kfolds/03a_k-folds_analysis.R /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_A_approx.rds /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_B_approx.rds /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/plot/kfold_approx
+"${script_dir}"/03_kfolds/03a_k-folds_analysis.R /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_A_raw.rds    /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_B_raw.rds    /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/plot/kfold_raw
 
 #---- Classify brikcs ----
 
-"${script_dir}"/04_classify/04_classify_bricks.R         /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/approx_samples_all_bands_csv.rds
-"${script_dir}"/04_classify/04_classify_bricks.R         /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/approx_samples_indices_csv.rds
-"${script_dir}"/04_classify/04_classify_partial_bricks.R /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/approx_samples_all_bands_csv.rds
-"${script_dir}"/04_classify/04_classify_partial_bricks.R /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/approx_samples_indices_csv.rds
+"${script_dir}"/04_classify/04_classify_bricks.R         /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_A_approx.rds
+"${script_dir}"/04_classify/04_classify_bricks.R         /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_B_approx.rds
+"${script_dir}"/04_classify/04_classify_partial_bricks.R /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_A_approx.rds
+"${script_dir}"/04_classify/04_classify_partial_bricks.R /home/alber/Documents/data/experiments/prodes_reproduction/papers/deforestation/data/validation/samples_B_approx.rds
 
 #---- Post processing ----
 
